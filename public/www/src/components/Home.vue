a5747001f6d0660465da6b4fbea94f49302ca069
jordanengstrom <jordan.engstrom@gmail.com>
2018-03-08 16:59:52 -0700

    styled home page

--- public/www/src/components/Home.vue
+++ public/www/src/components/Home.vue
@@ -1,39 +1,59 @@
 <template>
   <div class="Home">
     <navbar :user="user"></navbar>
-  <div>
-    <div>
-      <h2>Get aBoard you Boards</h2>
-      <div>
-          <!-- <i class="fas fa-2x fa-plus-square" @click="addBoard"></i> -->
-          <form @submit.prevent="addBoard()">
-            <input type="text" name="name" placeholder="Board Name" v-model="createdBoard.name">
-            <button type="submit" class="btn btn-submit">Create Board</button>
-          </form>
-      </div>
-    </div>
-
-    <div>
-      <div class="list-group">
-        <div v-for="board in boards" class="list-group-item">
-          <div>
-            <div>
-            <router-link :to="{path: '/board/' + board._id}" >
-              <h4>{{board.name}}</h4>
-            </router-link>
-              <i class="fas fa-minus-square pointer" @click="deleteBoard(board)"></i>
-              <!-- <i class="fas fa-pen-square" @click="editBoard(board)"></i> -->
+    <div class="bg">
+      <div class="container">
+        <div class="marg">
+          <div class="flexor">
+            <div class="boards-title">
+              <h2 class="planks-title">Your Planks</h2>
             </div>
-            <form @submit.prevent="editBoard">
-              <input type="text" :placeholder="board.name" name="name" value="">
-              <input type="text" :placeholder="board._id" name="id" :value="board._id" hidden>
-              <!-- <input class="" type="text" :value="board._id"> -->
-          
-              <button type="submit" class="brtn btn-submit">Submit</button>
-            </form>
+            <div class="aligner">
+              <div class="dropleft">
+                <button type="button" class="btn dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <i class="fas fa-plus"></i>
+                </button>
+                <div class="dropdown-menu">
+                  <form @submit.prevent="addBoard()">
+                    <input type="text" name="name" placeholder="Board Name" v-model="createdBoard.name">
+                    <!-- <button type="submit" class="btn btn-submit">Create Board</button> -->
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <div class="list-group">
+              <div v-for="board in boards" class="list-group-item">
+                <div class="flexor">
+                  <div>
+                    <router-link :to="{path: '/board/' + board._id}">
+                      <h4 class="board-links">
+                        <i class="fas fa-map-pin"></i> {{board.name}}</h4>
+                    </router-link>
+                  </div>
+                  <div class="flexor2">
+                    <transition-group name="" enter-active-class="animated flipInX" leave-active-class="animated fadeOut">
+                    <div :key="board._id" v-if="show == true && boardId == board._id">
+                        <form @submit.prevent="editBoard">
+                          <input type="text" placeholder="change board name" name="name" value="">
+                          <input type="text" :placeholder="board._id" name="id" :value="board._id" hidden>
+                          <button type="submit" class="btn" hidden>Change Name</button>
+                        </form>
+                      </div>
+                      <div :key="board.name" v-else></div>
+                    </transition-group>
+                    </div>
+                  <div class="faButtons">
+                    <i class="fas effect fa-wrench pointer" @click="showForm(board._id)"></i>
+                    <i class="far effect fa-minus-square pointer" @click="deleteBoard(board)"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-
       </div>
     </div>
   </div>
@@ -42,54 +62,171 @@
 
 <script>
   import Navbar from './Navbar'
-export default {
-  name: 'Home',
-  // This is temporary data that Home.vue and it's methods have access too.
-  // It's inaccessible outside of this file.
-  data () {
-    return {
-      createdBoard: {}
-    }
-  },
-  mounted(){
-    this.$store.dispatch('getBoards')
-  },
-  methods: {
-    getBoards(){
+  export default {
+    name: 'Home',
+    // This is temporary data that Home.vue and it's methods have access too.
+    // It's inaccessible outside of this file.
+    data() {
+      return {
+        createdBoard: {},
+        show: false,
+        boardId: ''
+      }
+    },
+    mounted() {
       this.$store.dispatch('getBoards')
     },
-    addBoard() {
-      this.$store.dispatch('addBoard',this.createdBoard)
+    methods: {
+      getBoards() {
+        this.$store.dispatch('getBoards')
+      },
+      addBoard() {
+        this.$store.dispatch('addBoard', this.createdBoard)
+        this.createdBoard = {}
+        // $('hide.bs.dropdown').dropdown()
+      },
+      deleteBoard(board) {
+        this.$store.dispatch('deleteBoard', board)
+      },
+      editBoard(event) {
+        var form = event.target
+        this.$store.dispatch('editBoard', { name: form.name.value, _id: form.id.value })
+        this.show = false
+      },
+      showForm(boardId) {
+        // local storage from returned data
+        this.formhidden
+        if (this.show) {
+          this.show = false
+        } else {
+          this.show = true
+        }
+        this.boardId = boardId
+      }
     },
-    deleteBoard(board) {
-      this.$store.dispatch('deleteBoard', board)
+    computed: {
+      boards() {
+        return this.$store.state.boards
+      },
+      user() {
+        return this.$store.state.user
+      }
     },
-    editBoard(event) {
-      var form = event.target
-      this.$store.dispatch('editBoard', {name: form.name.value, _id: form.id.value})
+    components: {
+      Navbar
     }
-  },
-  computed:{
-    boards(){
-      return this.$store.state.boards
-    },
-    user(){
-      return this.$store.state.user
-    }
-  },
-  components: {
-    Navbar
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .planks-title {
+    color: #6c6c6d;
+  }
+
+  .dropdown-toggle {
+    background-color: #797691;
+    color: #fff
+  }
+
   .pointer {
     cursor: pointer;
   }
-  .hidden{
+
+  .hidden {
     visibility: hidden;
   }
 
+  .boards-title {
+    text-align: center;
+    margin: 1.5rem 0;
+    font-family: 'Paytone One', sans-serif;
+  }
+
+  .flexor {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .flexor2 {
+    display: flex
+  }
+
+  .boards-title h2 {
+    font-weight: 700;
+  }
+
+  .aligner {
+    align-self: center;
+  }
+
+  .dropdown-menu {
+    padding: 0;
+  }
+
+  ::-webkit-input-placeholder {
+    text-align: center;
+  }
+
+  .dropdown-menu-show {
+    transform: translate3d(-187px, 2.5px, 0px) !important;
+  }
+
+  .faButtons {
+    font-size: 1rem;
+    /* color: #444444; */
+    /* color: #c69376; */
+    color: #5e796c;
+  }
+
+  .fa-wrench:hover {
+    transition: all 0.3s linear;
+    color: #ffee93;
+  }
+
+  .fa-minus-square:hover {
+    transition: all 0.3s linear;
+    color: #ce3c29
+  }
+
+  .bg {
+    background-image: url('../assets/vuekanban-bg.jpg');
+    background-size: cover;
+    background-position: center;
+  }
+
+  .effect {
+    transition: (all .5s ease-in-out)
+  }
+
+  .marg {
+    margin: 0 12rem;
+    padding-bottom: 3rem;
+  }
+
+  .board-links {
+    font-family: 'Quattrocento Sans', sans-serif;
+    color: #c69376;
+    font-weight: 700;
+    /* color: #6c948c; */
+    /* color: #5e796c; */
+  }
+
+  .list-group-item {
+    margin-bottom: 0.25rem;
+  }
+
+  .list-group-item:hover{
+    background-color: #f9f9f9;
+  }
+
+  .fa-map-pin {
+    padding-right: 1rem;
+    color: #797691;
+    /* color: #5e796c; */
+  }
+
+  .border-links h4 {
+    text-decoration: none;
+  }
 </style>
--- public/www/src/components/Navbar.vue
+++ public/www/src/components/Navbar.vue
@@ -1,18 +1,24 @@
 <template>
     <div>
         <nav class="navbar">
-            <div>
-                <h3>
-                    <i class="fas fa-map-pin"></i> All aBoard
-                </h3>
+            <div class="flexor">
+                <div>
+                    <img class="pad-r" src="../assets/ship-logo.png" height="50px">
+                </div>
+                <div>
+                    <h3>
+                        All aBoard
+                    </h3>
+                </div>
             </div>
             <div>
-                <div class="dropdown">
+                <div class="dropdown pointer">
                     <a class="dropdown-toggle" role="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <span><i class="fas fa-user"></i> {{user.username}}</span>
+                        <span>
+                            <i class="fas fa-user"></i> {{user.username}}</span>
                     </a>
                     <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-                        <button @click.prevent="logout" class="dropdown-item" type="button">logout</button>
+                        <button @click.prevent="logout" class="dropdown-item pointer" type="button">logout</button>
                     </div>
                 </div>
             </div>
@@ -36,8 +42,14 @@
 
 
 <style scoped>
+    .pointer {
+        cursor: pointer;
+    }
+
     .navbar {
-        background-color: #7BA6D0;
+        background-color: #797691;
+        /* background-color: #7BA6D0; */
+        color: white;
     }
 
     .navbar h3 {
@@ -45,9 +57,28 @@
         font-weight: 800;
         font-size: 2rem;
     }
+
     .dropdown-menu {
         min-width: 0;
         padding: .1rem 0;
         left: -2rem;
+    }
+
+    .dropdown-item:hover {
+        background-color:  #f9f9f9;
+    }
+
+    .flexor {
+        display: flex;
+        align-items: flex-end;
+        justify-content: space-around;
+    }
+
+    .flexor h3 {
+        margin-bottom: 0;
+    }
+
+    .pad-r {
+        padding-right: 1rem;
     }
 </style>
